7aa06a49199bbfb339489358b627d0cc0d804454
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-05-18 09:55:05 +0500

    SecVer

--- Практика2/Program.cs
+++ Практика2/Program.cs
@@ -41,21 +41,24 @@ namespace SecondTask
                     for(int g = 0; g < temp.Length; g++)
                     {
                         Second[i1, g] = Convert.ToInt32(temp[g]);
+                        for (int g1 = 0; g1 < n; g1++)
+                            Temp[g1, g] += First[g1, i1] * Second[i1, g];
                     }
                 }
-                for(int i2=0;i2<n;i2++)
-                    for(int i3 = 0; i3 < n; i3++)
-                    {
-                        Temp[i2, i3] = 0;
-                        for (int g = 0; g < n; g++)
-                            Temp[i2, i3] += First[i2, g] * Second[g, i3];
-                        Temp[i2, i3] %=p;
-                    }
+                //for(int i2=0;i2<n;i2++)
+                //    for(int i3 = 0; i3 < n; i3++)
+                //    {
+                //        Temp[i2, i3] = 0;
+                //        for (int g = 0; g < n; g++)
+                //            Temp[i2, i3] += First[i2, g] * Second[g, i3];
+                //        Temp[i2, i3] %=p;
+                //    }
                 for (int i1 = 0; i1 < n; i1++)
                 {
                     for (int g = 0; g < temp.Length; g++)
                     {
-                        First[i1, g] = Temp[i1, g];
+                        First[i1, g] = Temp[i1, g]%p;
+                        Temp[i1, g] = 0;
                     }
                 }
             }
