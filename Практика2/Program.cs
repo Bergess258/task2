3ec5b9c5185fe5e234ab0a7dd3212b7db0b0c84a
Bergess258 <parshakov-00@mail.ru>
2018-05-19 14:22:15 +0500

    dfg

--- Практика2/Program.cs
+++ Практика2/Program.cs
@@ -9,45 +9,54 @@ namespace SecondTask
         {
             StreamReader ForReading = new StreamReader("INPUT.TXT");
             string[] temp = ForReading.ReadLine().Split(' ');
-            int m = Convert.ToInt16(temp[0])-1, n = Convert.ToInt16(temp[1]);
+            int m = Convert.ToInt32(temp[0]) - 1, n = Convert.ToInt32(temp[1]);
             temp = ForReading.ReadLine().Split(' ');
-            int a= Convert.ToInt16(temp[0])-1, b = Convert.ToInt16(temp[1])-1;
-            int p = Convert.ToInt16(ForReading.ReadLine());
-            int[,] First = new int[n,n];
-            int[,] Temp = new int[n, n];
+            int a = Convert.ToInt32(temp[0]) - 1, b = Convert.ToInt32(temp[1]) - 1;
+            int p = Convert.ToInt32(ForReading.ReadLine());
+            int[][] First = new int[n][];
+            int[][] Temp = new int[n][];
             ForReading.ReadLine();
+            for (int i1 = 0; i1 < n; i1++)
+            {
+                First[i1] = new int[n];
+                Temp[i1] = new int[n];
+            }
             for (int i1 = 0; i1 < n; i1++)
             {
                 temp = ForReading.ReadLine().Split(' ');
                 for (int g = 0; g < temp.Length; g++)
                 {
-                    First[i1, g] = Convert.ToInt16(temp[g]);
+                    First[i1][g] = Convert.ToInt32(temp[g]);
                 }
             }
-            while(m--!=0)
+            for (int i = 0; i < m; i++)
             {
                 ForReading.ReadLine();
-                for(int i1 = 0; i1 < n; i1++)
+                for (int i1 = 0; i1 < n; i1++)
                 {
                     temp = ForReading.ReadLine().Split(' ');
-                    for(int g = 0; g < temp.Length; g++)
+                    for (int g = 0; g < temp.Length; g++)
                     {
                         int t = Convert.ToInt16(temp[g]);
                         for (int g1 = 0; g1 < n; g1++)
-                            Temp[g1, g] += First[g1, i1] * t;
+                            Temp[g1][g] += First[g1][i1] * t;
                     }
                 }
-                for (int i1 = 0; i1 < n; i1++)
+                for (int g = 0; g < n; g++)
                 {
-                    for (int g = 0; g < temp.Length; g++)
+                    for (int i1 = 0; i1 < temp.Length; i1++)
                     {
-                        First[i1, g] = Temp[i1, g]%p;
+                        First[g][i1] = Temp[g][i1] % p;
                     }
                 }
-                Temp = new int[n, n];
+                for (int g = 0; g < n; g++)
+                {
+                    Temp[g] = new int[n];
+                }
             }
+            ForReading.Close();
             StreamWriter Writel = new StreamWriter("OUTPUT.TXT");
-            Writel.Write(First[a,b]);
+            Writel.Write(First[a][b]);
             Writel.Close();
         }
     }
