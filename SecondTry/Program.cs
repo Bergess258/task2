085dba3e8789d756169763e070648517894c9208
Bergess258 <parshakov-00@mail.ru>
2018-05-20 23:10:42 +0500

    Полный писеч Что происходит Ничего не работает

---
+++ SecondTry/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SecondTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatrixMultiplication.myRecursiveTask.main(args);
+        }
+    }
+}
