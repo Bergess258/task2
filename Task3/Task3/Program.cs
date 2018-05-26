aa2585575e73b52eae5d8566009598b431506544
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-05-26 18:20:35 +0500

    оо

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using InputDLL;
+using MainLibrary;
 using System.Threading.Tasks;
 
 namespace Task3
@@ -11,7 +11,10 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            int x=;
+            int x=Library.ReadInt();
+            int y = Library.ReadInt();
+            if (4 * x * x + y * y <= 1) Console.WriteLine("Входит");
+            else Console.WriteLine("Не входит");
         }
     }
 }
