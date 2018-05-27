817e7052cfbcc243dcae289cd5680e67122febb7
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-05-27 19:22:40 +0500

    до

---
+++ RealTask4/RealTask4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> mas = new List<int>();
+            List<int> masForChanges = new List<int>();
+            mas.Add(1);
+            for(int i = 2; i < 101; i++)
+            {
+                Multiply(mas, masForChanges, i);
+            }
+        }
+
+        private static void Multiply(List<int> mas, List<int> masForChanges, int i)
+        {
+            for (int c = mas.Count; c > 0; --c)
+            {
+                int temp = mas[c] * i;
+                string tempS = temp.ToString();
+                mas[c] = tempS[tempS.Length - 1];
+                for (int i1 = tempS.Length - 1; i1 > 0; --i1)
+                    masForChanges.Add(tempS[i1]);
+            }
+        }
+    }
+}
---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] Matr = new int[8][];
+            for (int i = 0; i < Matr.Length; i++)
+                Matr[i] = new int[8];
+
+        }
+    }
+}
