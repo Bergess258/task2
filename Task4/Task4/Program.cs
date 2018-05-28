6d5b4eb2e100e5a728384340bcbfd79c92dfb422
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-05-28 13:49:19 +0500

    Ready5Task

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MainLibrary;
 using System.Threading.Tasks;
 
 namespace Task4
@@ -10,10 +11,50 @@ namespace Task4
     {
         static void Main(string[] args)
         {
-            int[][] Matr = new int[8][];
-            for (int i = 0; i < Matr.Length; i++)
-                Matr[i] = new int[8];
-
+            int n = 8;
+            int[] Numbers = new int[n * n];
+            int[][] Matr = new int[n][];
+            for (int i1 = 0; i1 < n; i1++)
+                Matr[i1] = new int[n];
+            for (int i1 = 0; i1 < Numbers.Length; i1++)
+                Numbers[i1] = Library.RandIntFT(0, 100);
+            int i = 0, j = 0, c = 1, h = 0;
+            Matr[0][0] = Numbers[0];
+            for (int i1 = 1; i1 <= n; i1++)
+                h += i1;
+            while (true)
+            {
+                Matr[i][++j] = Numbers[c++];
+                LeftDown(n, Numbers, Matr, ref i, ref j, ref c);
+                if (i == n-1 && j == 0) break;
+                Matr[++i][j] = Numbers[c++];
+                RightUp(n, Numbers, Matr, ref i, ref j, ref c);
+                
+            }
+            while (true)
+            {
+                Matr[i][++j] = Numbers[c++];
+                RightUp(n, Numbers, Matr, ref i, ref j, ref c);
+                if (i == n - 1 && j == i) break;
+                Matr[++i][j] = Numbers[c++];
+                LeftDown(n, Numbers, Matr, ref i, ref j, ref c);
+            }
+            Library.WriteMass(Numbers);
+            Library.WriteMass(Matr);
+        }
+        private static void LeftDown(int n, int[] Numbers, int[][] Matr, ref int i, ref int j, ref int c)
+        {
+            while (i < n - 1 && j > 0)
+            {
+                Matr[++i][--j] = Numbers[c++];
+            }
+        }
+        private static void RightUp(int n, int[] Numbers, int[][] Matr, ref int i, ref int j, ref int c)
+        {
+            while (i >0 && j < n-1)
+            {
+                Matr[--i][++j] = Numbers[c++];
+            }
         }
     }
 }
