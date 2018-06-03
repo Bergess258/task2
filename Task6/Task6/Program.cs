75c6225792f8f47c062937767b9b350fb720e8bf
Bergess <Berges@BERGESS>
2018-06-03 19:58:38 +0500

    ReadySevenh

--- Task6/Task6/Program.cs
+++ Task6/Task6/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MainLibrary;
 using System.Threading.Tasks;
 
 namespace Task6
@@ -10,7 +11,23 @@ namespace Task6
     {
         static void Main(string[] args)
         {
-
+            List<double> mas = new List<double>();
+            mas.Add(Library.ReadDouble());
+            mas.Add(Library.ReadDouble());
+            mas.Add(Library.ReadDouble());
+            double M = Library.ReadDouble();
+            int c = 2;
+            while (mas[c] < M)
+            {
+                mas.Add(mas[c] * mas[c - 1] + mas[c - 2]);
+                c++;
+            }
+            foreach (double t in mas)
+                Console.Write(t + " ");
+            Console.WriteLine();
+            Console.Write("Равенство ");
+            if (mas[mas.Count - 1] == M) Console.WriteLine("выполняется");
+            Console.WriteLine("не выполняется");
         }
     }
 }
---
+++ Zadanie7/Zadanie7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MainLibrary;
+using System.Threading.Tasks;
+
+namespace Zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ok;
+            string first;
+            List<byte> list = new List<byte>();
+            do
+            {
+                ok = true;
+                first = Console.ReadLine();
+                foreach(char y in first)
+                {
+                    string y1 = Convert.ToString(y);
+                    byte t = Convert.ToByte(y1);
+                    if (t < 0 && t > 1) { Console.WriteLine("Неверный формат кода"); ok = false; break; }
+                    list.Add(t);
+                } 
+            } while (ok == false);
+            int Step = 2;
+            List<byte> End = new List<byte>();
+            End.Add(0);
+            int c = 2;
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (c++ == Step) { End.Add(0);Step *= 2;c++; }
+                End.Add(list[i]);
+            }
+            for (int i = 1; i < Step; i*=2)
+            {
+                int temp = 0;
+                int g = i-1;
+                while (g < End.Count)
+                {
+                    for (int t = 0; t < i; t++)
+                        if (g + t < End.Count)
+                            temp += End[g + t];
+                        else break;
+                    g += i * 2;
+                }
+                if (temp % 2 == 1) End[i-1] = 1;
+            }
+            foreach (byte s in End)
+                Console.Write(s);
+            Console.WriteLine();
+        }
+    }
+}
