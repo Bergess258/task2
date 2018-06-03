3d1b67149ca9fada3189cb0456208081a387a2b3
Bergess <Berges@BERGESS>
2018-06-03 11:11:33 +0500

    ready4task

--- RealTask4/RealTask4/Program.cs
+++ RealTask4/RealTask4/Program.cs
@@ -17,17 +17,36 @@ namespace RealTask4
             {
                 Multiply(mas, masForChanges, i);
             }
+            foreach (int t in mas)
+                Console.Write(t);
         }
         private static void Multiply(List<int> mas, List<int> masForChanges, int i)
         {
-            for (int c = mas.Count; c > 0; --c)
+            for (int c = 0; c < mas.Count; c++)
             {
                 int temp = mas[c] * i;
-                string tempS = temp.ToString();
-                mas[c] = tempS[tempS.Length - 1];
-                for (int i1 = tempS.Length - 1; i1 > 0; --i1)
-                    masForChanges.Add(tempS[i1]);
+                masForChanges.Add(temp);
             }
+            for(int c = 0; c < masForChanges.Count; c++)
+            {
+                if (masForChanges[c] > 9)
+                {
+                    int temp = masForChanges[c];
+                    masForChanges[c] = temp % 10;
+                    if (masForChanges.Count == c + 1) masForChanges.Add(temp/10);
+                    else
+                        masForChanges[c+1] +=temp/10;
+                }
+            }
+            for (int c = 0; c < masForChanges.Count; c++)
+            {
+                int temp= masForChanges[c];
+                if (c < mas.Count)
+                    mas[c] = temp;
+                else
+                    mas.Add(temp);
+            }
+            masForChanges.Clear();
         }
     }
 }
