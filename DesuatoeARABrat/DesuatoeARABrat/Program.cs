7389dc3a1654704d176c62e9db9b59e5a971e0e3
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-06-20 19:41:44 +0500

    RadyDESUATOEMOYO

---
+++ DesuatoeARABrat/DesuatoeARABrat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainLibrary;
+
+namespace DesuatoeARABrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Library.ReadIntMoreThan(0);
+            int m = Library.ReadIntMoreThan(0);
+            int c = m;
+            List list = new List(n);
+            list.StartSchet(m);
+            Console.WriteLine(list.Data());
+        }
+    }
+}
