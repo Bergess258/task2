05e7cf349cdcb48e4ed5a986fd9159d5dbe20ee2
Виталий Паршаков <vaparshakov@edu.hse.ru>
2018-06-24 13:57:31 +0500

    в сб

---
+++ MainLibrary/MainLibrary/CollectionHandlerEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    public class CollectionHandlerEventArgs: EventArgs
+    {
+        public string Name { get; set; }
+
+        public string ChangeName { get; set; }
+
+        public object Obj { get; set; }
+
+        public CollectionHandlerEventArgs(string name, string change, object obj)
+        {
+            Name = name;
+            ChangeName = change;
+            Obj = obj;
+        }
+
+        public override string ToString()
+        {
+            return "Название коллекции: " + Name + "\nСобытие: " + ChangeName + "\n Объект: " + Obj.ToString();
+        }
+    }
+}
